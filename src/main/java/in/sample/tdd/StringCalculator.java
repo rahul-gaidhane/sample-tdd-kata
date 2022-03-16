aaf4f612772089dbc4e94c861e78a8352dc2d08f
rahul-gaidhane <rahulgaidhane98@gmail.com>
2022-03-16 03:12:34 +0000

    handle the empty string

--- src/main/java/in/sample/tdd/StringCalculator.java
+++ src/main/java/in/sample/tdd/StringCalculator.java
@@ -2,8 +2,11 @@ package in.sample.tdd;
 
 public class StringCalculator {
 
-	public static Integer Add(String string) {
+	public static Integer Add(String numbers) {
 		
+		if(numbers.isBlank()) {
+			return 0;
+		}
 		return null;
 	}
 
