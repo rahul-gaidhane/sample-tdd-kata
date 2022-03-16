2609d6e4814c7ee37677fa52a2d573ec70e6d567
rahul-gaidhane <rahulgaidhane98@gmail.com>
2022-03-16 03:22:31 +0000

    rename test method

--- src/test/java/in/sample/tdd/StringCalculatorTest.java
+++ src/test/java/in/sample/tdd/StringCalculatorTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 public class StringCalculatorTest {
 	
 	@Test
-	public void testStringCalculatorMethod() {
+	public void testStringCalculatorMethodScenario1() {
 		assertEquals(0, StringCalculator.Add(""));
 		assertEquals(1, StringCalculator.Add("1"));
 		assertEquals(3, StringCalculator.Add("1,2"));
