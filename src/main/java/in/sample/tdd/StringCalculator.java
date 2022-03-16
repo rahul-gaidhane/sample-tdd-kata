20eefab9194ac4a5f0eb58896dbc3ad9a06aab4d
rahul-gaidhane <rahulgaidhane98@gmail.com>
2022-03-16 04:10:55 +0000

    handle the negative nums

--- src/main/java/in/sample/tdd/StringCalculator.java
+++ src/main/java/in/sample/tdd/StringCalculator.java
@@ -1,11 +1,13 @@
 package in.sample.tdd;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class StringCalculator {
 
-	public static Integer Add(String numbers) {
+	public static Integer Add(String numbers) throws Exception {
 		
 		if(numbers.isBlank()) {
 			return 0;
@@ -22,6 +24,8 @@ public class StringCalculator {
 		
 		int sum = 0;
 		
+		List<String> negativeNums = new ArrayList<>();
+		
 		for(String commaSepNum : commaSepNumArr) {
 			
 			if(commaSepNum.contains("\n")) {
@@ -29,14 +33,26 @@ public class StringCalculator {
 				
 				for(String newLineNum : newLineSepArr) {
 					Integer nlNum = Integer.parseInt(newLineNum);
+					
+					if(nlNum < 0) {
+						negativeNums.add(newLineNum);
+					}
 					sum += nlNum;
 				}
 			} else {
 				Integer cNum = Integer.parseInt(commaSepNum);
+				
+				if(cNum < 0) {
+					negativeNums.add(commaSepNum);
+				}
 				sum += cNum;
 			}
 		}
 		
+		if(negativeNums.size() > 0) {
+			String negativeNumsString = negativeNums.stream().collect(Collectors.joining(","));
+			throw new Exception("negatives not allowed " + negativeNumsString);
+		}
 		return sum;
 	}
 
--- src/test/java/in/sample/tdd/StringCalculatorTest.java
+++ src/test/java/in/sample/tdd/StringCalculatorTest.java
@@ -1,35 +1,41 @@
 package in.sample.tdd;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
 public class StringCalculatorTest {
 	
 	@Test
-	public void testStringCalculatorMethodScenario1() {
+	public void testStringCalculatorMethodScenario1() throws Exception {
 		assertEquals(0, StringCalculator.Add(""));
 		assertEquals(1, StringCalculator.Add("1"));
 		assertEquals(3, StringCalculator.Add("1,2"));
 	}
 	
 	@Test
-	public void testStringCalculatorMethodScenario2() {
+	public void testStringCalculatorMethodScenario2() throws Exception {
 		assertEquals(15, StringCalculator.Add("1,2,3,4,5"));
 		assertEquals(54321, StringCalculator.Add("1,20,300,4000,50000"));
 	}
 	
 	@Test
-	public void testStringCalculatorMethodScenario3() {
+	public void testStringCalculatorMethodScenario3() throws Exception {
 		assertEquals(6, StringCalculator.Add("1\n2,3"));
 		assertEquals(54321, StringCalculator.Add("1,20\n300,4000\n50000"));
 		assertEquals(15, StringCalculator.Add("1,2\n3,4\n5"));
 	}
 	
 	@Test
-	public void testStringCalculatorMethodScenario4() {
+	public void testStringCalculatorMethodScenario4() throws Exception {
 		assertEquals(3, StringCalculator.Add("//;\n1;2"));
 		assertEquals(54321, StringCalculator.Add("//:\n1:20\n300:4000\n50000"));
 		assertEquals(15, StringCalculator.Add("//>\n1>2\n3>4\n5"));
+	}
+	
+	@Test
+	public void testStringCalculatorMethodScenario5() {
+		assertThrows(Exception.class, () -> StringCalculator.Add("//;\n1;-2"));
 	}
 }
