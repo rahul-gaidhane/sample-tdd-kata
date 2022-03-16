985966cdcb9e4d47414bba07c7399f133b9902e2
rahul-gaidhane <rahulgaidhane98@gmail.com>
2022-03-16 03:00:57 +0000

    initial commit

---
+++ src/main/java/in/sample/tdd/TddSampleApplication.java
@@ -0,0 +1,13 @@
+package in.sample.tdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/sample/tdd/TddSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package in.sample.tdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
